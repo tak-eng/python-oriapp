76f2f64663b5831c2dc43c4849717aed5d531a99
tak-eng <shibamoto2411@gmail.com>
2021-02-01 01:41:50 +0900

    settings.pyファイルにDB設計を追記

--- newsproject/settings.py
+++ newsproject/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'newsproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
